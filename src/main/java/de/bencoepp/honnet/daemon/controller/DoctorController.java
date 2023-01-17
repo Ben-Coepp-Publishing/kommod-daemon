65a1ad9bf828f67ed178b72d1968f1ecfe90cf96
Ben Coepp <bencoepp@gmail.com>
2023-01-17 20:19:25 +0100

    added base doctor all endpoint

---
+++ src/main/java/de/bencoepp/honnet/daemon/controller/DoctorController.java
@@ -0,0 +1,20 @@
+package de.bencoepp.honnet.daemon.controller;
+
+import de.bencoepp.entity.Check;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/api/doctor")
+public class DoctorController {
+
+    @GetMapping("/all")
+    public ResponseEntity<ArrayList<Check>> getAllChecks(){
+        ArrayList<Check> checks = new ArrayList<>();
+        return ResponseEntity.ok(checks);
+    }
+}
