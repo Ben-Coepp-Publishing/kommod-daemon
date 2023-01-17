1d5cfc8b5275b736d209bd8d707cd9e599c1df8c
Ben Coepp <bencoepp@gmail.com>
2023-01-17 19:54:27 +0100

    init commit

---
+++ src/main/java/de/bencoepp/honnet/daemon/HonnetDaemonApplication.java
@@ -0,0 +1,13 @@
+package de.bencoepp.honnet.daemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonnetDaemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HonnetDaemonApplication.class, args);
+    }
+
+}
