ec6d3b3d8e29b5e0340b6ceb7078b2243727a1a9
Ben Cöppicus <ben.coeppicus@gus-group.com>
2023-01-18 15:30:37 +0100

    added first base network test to config and used find all for finding all files in the test directory

--- src/main/java/de/bencoepp/honnet/daemon/controller/TestController.java
+++ src/main/java/de/bencoepp/honnet/daemon/controller/TestController.java
@@ -1,12 +1,15 @@
 package de.bencoepp.honnet.daemon.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import de.bencoepp.entity.test.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Array;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 @Controller
@@ -14,8 +17,15 @@ import java.util.ArrayList;
 public class TestController {
 
     @GetMapping("/all")
-    public ResponseEntity<ArrayList<Test>> getAllTests(){
+    public ResponseEntity<ArrayList<Test>> getAllTests() throws IOException {
         ArrayList<Test> tests = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        File dir = new File("tests");
+        File[] files = dir.listFiles();
+        for (File file : files){
+            Test test = mapper.readValue(new File(file.getCanonicalPath()), Test.class);
+            tests.add(test);
+        }
         return ResponseEntity.ok(tests);
     }
 }
