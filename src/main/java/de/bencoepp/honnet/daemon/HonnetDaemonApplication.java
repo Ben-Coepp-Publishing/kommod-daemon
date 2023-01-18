dd5dae1f1a91e6eafa9c7a9229703c100496276d
Ben Cöppicus <ben.coeppicus@gus-group.com>
2023-01-18 15:40:18 +0100

    added scheduler

--- src/main/java/de/bencoepp/honnet/daemon/HonnetDaemonApplication.java
+++ src/main/java/de/bencoepp/honnet/daemon/HonnetDaemonApplication.java
@@ -2,8 +2,10 @@ package de.bencoepp.honnet.daemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HonnetDaemonApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/de/bencoepp/honnet/daemon/scheduler/Scheduler.java
@@ -0,0 +1,47 @@
+package de.bencoepp.honnet.daemon.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Component
+public class Scheduler {
+
+    @Scheduled(fixedRate = 60000)
+    public void executeScheduler() throws InterruptedException {
+        new Thread(() ->{
+            Class<?> clazz = null;
+            try {
+                clazz = Class.forName("");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            Constructor<?> constructor = null;
+            try {
+                constructor = clazz.getConstructor();
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+            Object instance = null;
+            try {
+                instance = constructor.newInstance();
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+            Method method;
+            try {
+                method = instance.getClass().getMethod("execute");
+                method.invoke(instance);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }){{start();}}.join();
+    }
+}
