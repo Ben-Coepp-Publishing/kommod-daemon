5d0c1753a4d105887d54857e2e08b9f0f3934833
Ben Cöppicus <ben.coeppicus@bencoepp.de>
2023-01-20 15:21:40 +0000

    updated names

--- src/main/java/de/bencoepp/honnet/daemon/KommodDaemonApplication.java
+++ src/main/java/de/bencoepp/honnet/daemon/KommodDaemonApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class HonnetDaemonApplication {
+public class KommodDaemonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HonnetDaemonApplication.class, args);
+        SpringApplication.run(KommodDaemonApplication.class, args);
     }
 
 }
