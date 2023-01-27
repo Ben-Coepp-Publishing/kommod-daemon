df3be486e8cfa1b7c49f47c6d1e76db1b5cada52
Ben Coepp <bencoepp@gmail.com>
2023-01-27 21:30:27 +0100

    update

--- src/main/java/de/bencoepp/kommod/daemon/controller/ReportController.java
+++ src/main/java/de/bencoepp/kommod/daemon/controller/ReportController.java
@@ -3,7 +3,6 @@ package de.bencoepp.kommod.daemon.controller;
 import de.bencoepp.entity.test.Report;
 import de.bencoepp.kommod.daemon.repository.ScheduledTestRepository;
 import de.bencoepp.kommod.daemon.utils.ReportHelper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
